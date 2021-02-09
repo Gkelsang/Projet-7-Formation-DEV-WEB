0e3c177101fab18a2d1dc234fcf5544e3950412c
Gkelsang <g.kelsang@yahoo.fr>
2021-02-09 14:44:02 +0100

    change router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,26 @@
+// Importations //
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import auth from '../middleware/auth'
+import VueRouteMiddleware from 'vue-route-middleware'
 
 Vue.use(VueRouter)
 
+// Routes //
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', name: 'Posts', component: () => import('../views/Posts.vue'), meta: { middleware: auth } },
+  { path: '/posts/:postId', name: 'OnePost', component: () => import('../views/OnePost.vue'), meta: { middleware: auth } },
+  { path: '/login', name: 'Login', component: () => import('../views/Login.vue') },
+  { path: '/signup', name: 'Signup', component: () => import('../components/Signup.vue')},
+  { path: '/profile', name: 'Profile', component: () => import('../views/Profile.vue'), meta: { middleware: auth } },
+  { path: '/profile/:userId', name: 'UserProfile', component: () => import('../views/UserProfile.vue'), meta: { middleware: auth } }
 ]
 
 const router = new VueRouter({
   routes
 })
 
+router.beforeEach(VueRouteMiddleware())
+
+// Exportations du router //
 export default router
