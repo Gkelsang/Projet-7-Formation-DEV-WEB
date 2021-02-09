dcfbe0fd59737c08041158dc1e31f0519daabac6
Gkelsang <g.kelsang@yahoo.fr>
2021-02-09 14:43:51 +0100

    inital commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav" class="d-flex sticky-top justify-content-start justify-content-sm-center mb-3 p-3">
+            <router-link to="/">
+                <img id="logo" 
+                :src="require('./assets/logos/icon-left-font-monochrome-white2.png')"
+                alt="logo Groupomania" />
+            </router-link>
+        </div>
+        <NotificationAlert />
+        <router-view />
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NotificationAlert from '../src/components/NotificationAlert'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+    name: 'App',
+    components: {
+        NotificationAlert
     }
-  }
 }
-</style>
+</script>
